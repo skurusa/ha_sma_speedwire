04de986269192a0d1a7e772654f9acd4e63b6734
Sandor Kurusa <92585505+skurusa@users.noreply.github.com>
2026-08-27 15:37:56 +0200

    Add configurable polling interval constants

--- custom_components/sma_speedwire/const.py
+++ custom_components/sma_speedwire/const.py
@@ -1,3 +1,8 @@
 """Constants for the sma_speedwire integration."""
 
 DOMAIN = "sma_speedwire"
+
+CONF_SCAN_INTERVAL = "scan_interval"
+DEFAULT_SCAN_INTERVAL = 30
+MIN_SCAN_INTERVAL = 10
+MAX_SCAN_INTERVAL = 300
